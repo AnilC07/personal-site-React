d8cc7da72d3b87bd748b6dfe5dd07b4aaae7e018
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-02-18 07:30:14 +0100

    ajout de la classe active au click

--- src/components/UI/FilterButton.js
+++ src/components/UI/FilterButton.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-
-import './FilterButton.css'
+import "./FilterButton.css";
 
 const FilterButton = ({ competence, id, filterOnClick, selected }) => {
   return (
-      <button id="filter" 
-      className={selected.join('').includes(competence) && 'active'} 
-      value={competence} 
-      onClick={filterOnClick}>{competence}</button>
+    <button
+      id="filter"
+      className={selected.join("").includes(competence) && "active"}
+      value={competence}
+      onClick={filterOnClick}
+    >
+      {competence}
+    </button>
   );
 };
 
