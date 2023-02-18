8e544a0867ec38868771890aae9a0258173a4190
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-02-18 07:30:53 +0100

    modification mineure

--- src/data.js
+++ src/data.js
@@ -57,7 +57,7 @@ export const projets = [
     titre: "Lorem Ipsum 4",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     lien: "www.lorem.com",
-    tags: ["Next.js"],
+    tags: ["NextJS"],
   },
   {
     id: 5,
