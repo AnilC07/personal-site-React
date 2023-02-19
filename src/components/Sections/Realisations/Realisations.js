9ff7c3bcd6fc9342c4f697afe3ba4fcda13dfb5a
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-02-19 11:12:30 +0100

    fix multiple selection

--- src/components/Sections/Realisations/Realisations.js
+++ src/components/Sections/Realisations/Realisations.js
@@ -5,55 +5,96 @@ import { competences, projets } from "../../../data";
 
 import "./Realisations.css";
 import ProjectCard from "../../UI/ProjectCard";
-import useCtx from "../../../Ctx";
+
 import { useEffect, useState } from "react";
 
 const Realisations = () => {
-  // const context = useCtx();
   const [filteredProject, setFilteredProject] = useState(projets);
-  const [selectedCompetence, setSelectedCompetence] = useState([])
+  // const [selectedCompetence, setSelectedCompetence] = useState([
+  //   "Tous les projets",
+  // ]);
+  const [selectedCompetence, setSelectedCompetence] = useState([]);
+  // console.log([] === null);
 
   /**
-   * Fonction qui filtre les projets suivant les compétence selectionnées
-   * @param {*} e 
+   * Fonction qui set les compétences selectionnées
+   * @param {*} e
    */
-  const filter = (e) => {
+  const setProjectsToDisplay = (e) => {
     // Récupere la valeur correspondante au boutton clické
     const selectedTag = e.target.value;
 
     // Verifier si la valeur clické et deja présente
-    if(selectedCompetence.includes(selectedTag)){
-    // Si oui, on la retire de la variable d'état
-      setSelectedCompetence(prev=>prev.filter(el=>el !== selectedTag))
-  }else{
-    // Sinon, on rajoute la nouvelle valeur
-    // Injecte la valeur du boutton selectionée dans une variable d'état
-    setSelectedCompetence((prev)=>[...prev, selectedTag])
-  }
+    if (selectedCompetence.includes(selectedTag)) {
+      // Si oui, on la retire de la variable d'état
+      setSelectedCompetence((prev) => prev.filter((el) => el !== selectedTag));
+    } else {
+      // Sinon, on rajoute la nouvelle valeur
+      // Injecte la valeur du boutton selectionée dans une variable d'état
+      setSelectedCompetence((prev) => [...prev, selectedTag]);
+    }
+    //         if(selectedCompetence.join(" ").includes(selectedCompetence) !== 'Tous les projets'){
+    // console.log('hello world')
 
-  // Filtre les projets à affichés
-  // Verifie la valeur du boutton clické
-    if(selectedTag === 'Tous les projets' || selectedTag === []){
-      // Si oui, inject tous les projets dans la variable d'état. État par défault.
-      setFilteredProject(projets)
-    }else{
-      // Si différent de 'Tous les projets', filtre les projets ayant le tag associé à la valeur du boutton
-    const projetFiltrer = projets.filter((el) => el.tags.join(" ").includes(selectedTag));
-    setFilteredProject(projetFiltrer)}
+    //     }
+    if (selectedCompetence === []) {
+      setSelectedCompetence(["Tous les projets"]);
+    }
   };
 
-  useEffect(()=>{
+  /**
+   * Fonction qui filtre les projets suivant les compétence selectionnées
+   * @param {[String]} selectedTag
+   */
+  const filter = (selectedCompetence) => {
+    // Verifie la valeur du boutton clické
 
-  },[filteredProject,selectedCompetence])
+    if (selectedCompetence[0] === "Tous les projets") {
+      // Si oui, inject tous les projets dans la variable d'état. État par défault.
+      setFilteredProject(projets);
+    }
+    if (selectedCompetence === []) {
+      // Si oui, inject tous les projets dans la variable d'état. État par défault.
+      setSelectedCompetence(["Tous les projets"]);
+      setFilteredProject(projets);
+    }
+
+    if (selectedCompetence.length > 1) {
+      // Si selectedCompetence est un array avec plus d'1 elements, on boucle dessus et ajoute les projets correspondant dans la variable d'état
+      let arr = [];
+      selectedCompetence.map((competence) => {
+       return arr.push(
+          projets.filter((el) => el.tags.join(" ").includes(competence))
+        );
+        //     // return setFilteredProject((prev) =>[...prev, competence.filter(el=>el.tags.join(" ").includes(selectedCompetence) === competence)])
+      });
+      arr= arr.flat();
+      console.log(arr)
+      setFilteredProject(arr);
+    } else {
+      // Si différent de 'Tous les projets', filtre les projets ayant le tag associé à la valeur du boutton
+      const projetFiltrer = projets.filter((el) =>
+        el.tags.join(" ").includes(selectedCompetence)
+      );
+      setFilteredProject(projetFiltrer);
+    }
+  };
+
+  useEffect(() => {
+    // setSelectedCompetence(["Tous les projets"]);
+    filter(selectedCompetence);
+  }, []);
+
+  useEffect(() => {
+    filter(selectedCompetence);
+  }, [selectedCompetence]);
 
   return (
     <section id="realisations">
       <SectionHeading>Réalisations</SectionHeading>
       <div className="projects-container">
         <div className="filter">
-          {/* <label htmlFor="filter">Flitrer par compétences&nbsp;:&nbsp;</label> */}
           <div className="filter-btn">
-            {/* <select style={{display:'hidden'}}> */}
             {competences.map((el) => {
               const { id, competence } = el;
               return (
@@ -61,14 +102,13 @@ const Realisations = () => {
                   key={id}
                   id="filter"
                   competence={competence}
-                  filterOnClick={filter}
+                  filterOnClick={setProjectsToDisplay}
                   selected={selectedCompetence}
                 />
               );
             })}
           </div>
           <div className="custom-select">
-            {/* <label htmlFor="filter">Flitre&nbsp;:&nbsp;</label> */}
             <select>
               {competences.map((el) => {
                 const { id, competence } = el;
--- src/data.js
+++ src/data.js
@@ -33,7 +33,7 @@ export const projets = [
     titre: "Lorem Ipsum 1",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     lien: "www.lorem.com",
-    tags: ["ReactJS", "NodeJS"],
+    tags: [ "NodeJS"],
   },
   {
     id: 2,
@@ -49,7 +49,7 @@ export const projets = [
     titre: "Lorem Ipsum 3",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     lien: "www.lorem.com",
-    tags: ["ReactJS", "NodeJS"],
+    tags: ["Bootstrap"],
   },
   {
     id: 4,
