2a015d0b7c6ec7b0a7da897d162f981765a349a1
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-02-19 13:36:38 +0100

    beautify code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,18 @@ import Navbar from '../Navbar/Navbar'
 import './Header.css'
 
 const Header = () => {
-  return (<div id='header'>
+  return (<div id='header' className='background-wrap'>
     <Navbar />
     <div className="header-container">
         <h1>Anil CELIK</h1>
         <h2>Programmeur junior JavaScript.</h2>
       </div>
-      </div>
+      <div class="bubble x1"><img src='/images/js2.png' /></div>
+    <div class="bubble x2"><img src='/images/next.png' /></div>
+    <div class="bubble x3"><img src='/images/node2.png' /></div>
+    <div class="bubble x4"><img src='/images/react-js.png' /></div>
+    <div class="bubble x5"><img src='/images/typescript.png' /></div>
+    </div>
   )
 }
 
