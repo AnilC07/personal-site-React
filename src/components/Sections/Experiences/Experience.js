72382b6ec88b726043c4c3762bace9f2437a2d57
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-02-19 11:15:46 +0100

    fix padding on the last item

--- src/components/Sections/Experiences/Experience.js
+++ src/components/Sections/Experiences/Experience.js
@@ -1,44 +1,48 @@
-import React from 'react'
+import React from "react";
 
 import Logo from "../../Logo";
 
 import Detail from "./Detail";
 
-const Experience = ({ id, company, poste, logo, debut, end, presentation, tasks }) => {
-    
-    
-
-
-  return (<div className="experience" key={id}>
-            <div className="experience__header-container">
-              <div className="header">
-                <div style={{ overflow: "hidden" }}>
-                  <Logo logo={logo} company={company} />
-                </div>
-                <div className="informations">
-                  <h3>
-                    {company} - {poste}
-                  </h3>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      marginLeft: "auto",
-                    }} >
-                    <p style={{  paddingRight: "1rem" }}>
-                      {debut} {end && <span>- {end}</span>}
-                    </p>
-                  </div>
-                </div>
-            
-
-              </div>
-              <Detail presentation={presentation} tasks={tasks} />
-            </div>
-            
+const Experience = ({
+  id,
+  company,
+  poste,
+  logo,
+  debut,
+  end,
+  presentation,
+  tasks,
+}) => {
+  return (
+    <div className="experience" key={id}>
+      <div className="experience__header-container">
+        <div className="header">
+          <div style={{ overflow: "hidden" }}>
+            <Logo logo={logo} company={company} />
           </div>
-        );
-}
+          <div className="informations">
+            <h3>
+              {company} - {poste}
+            </h3>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginLeft: "auto",
+              }}
+            >
+              <p style={{ paddingRight: "1rem" }}>
+                {debut} {end && <span>- {end}</span>}
+              </p>
+            </div>
+          </div>
+        </div>
+        <Detail presentation={presentation} tasks={tasks} />
+      </div>
+    </div>
+  );
+};
 
-export default Experience
+export default Experience;
